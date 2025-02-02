f7aa08d2007d7203a365b58f2af92ca878e4014a
Amanhost <amanprakash1639@gmail.com>
2025-02-02 13:09:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DraftEditor from "./Component/DraftEditor";
+
+const App = () => {
+  return (
+    <div>
+      <DraftEditor />
+    </div>
+  );
+};
+
+export default App;
