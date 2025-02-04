5aabc3c8cb37f48e8748b1392bb8d53ce6921a3b
Amanhost <amanprakash1639@gmail.com>
2025-02-04 17:27:49 +0530

    second chanegs

---
+++ src/Component/Product.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Product = ({ value, index, handleDelete, handleEdit }) => {
+  return (
+    <tr key={index}>
+      <td>{value.name}</td>
+      <td>{value.date}</td>
+      <td>{value.amount}</td>
+      <td>
+        <button onClick={() => handleEdit(index)}>Edit</button>
+        <button onClick={() => handleDelete(index)}>Delete</button>
+      </td>
+    </tr>
+  );
+};
+
+export default Product;
